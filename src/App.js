a7dba199680c0b44cc62eab3d4a041280b119ae8
Martín Calderón <martincalderon92@outlook.com>
2021-10-06 16:12:38 -0300

    :sparkles: Add footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import CardsContainer from './components/CardsContainer';
 import Navbar from './components/Navbar';
+import CardsContainer from './components/CardsContainer';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			<CardsContainer />
+			<Footer />
 		</div>
 	);
 }
