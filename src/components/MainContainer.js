53654f2e1f96d7279043b01ff7ebe34536801a26
Martín Calderón <martincalderon92@outlook.com>
2021-10-06 23:15:36 -0300

    :sparkles: Add cards pagination and conditional rendering of the chevrons.

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,8 @@ import Chevron from './Chevron';
 import Card from './Card';
 
 export default function MainContainer() {
-	const fetchUrl = 'https://api.rawg.io/api/games?page_size=8&key=' + API_KEY;
+	const url = 'https://api.rawg.io/api/games?page_size=8&key=' + API_KEY;
+	const [fetchUrl, setFetchUrl] = useState(url);
 	const [posts, setPosts] = useState(undefined);
 	const [loading, setLoading] = useState(false);
 
@@ -25,26 +26,44 @@ export default function MainContainer() {
 		getPosts();
 	}, [fetchUrl]);
 
+	const handlePreviousClick = () => {
+		setFetchUrl(posts.previous);
+	};
+
+	const handleNextClick = () => {
+		setFetchUrl(posts.next);
+	};
+
 	return (
 		<div className={styles.mainContainer}>
-			<Chevron
-				className={styles.previous}
-				// onClick={handlePreviousClick}
-				orientation="left"
-			/>
+			{posts?.previous && (
+				<Chevron
+					className={styles.previous}
+					onClick={handlePreviousClick}
+					orientation="left"
+				/>
+			)}
+
 			<div className={styles.cardsContainer}>
 				{loading && <h1>Loading...</h1>}
 
-				{posts?.results.map((post) => (
-					<Card key={post.id} name={post.name} image={post.background_image} />
-				))}
+				{!loading &&
+					posts?.results.map((post) => (
+						<Card
+							key={post.id}
+							name={post.name}
+							image={post.background_image}
+						/>
+					))}
 			</div>
 
-			<Chevron
-				className={styles.next}
-				// onClick={handleNextClick}
-				orientation="right"
-			/>
+			{posts?.next && (
+				<Chevron
+					className={styles.next}
+					onClick={handleNextClick}
+					orientation="right"
+				/>
+			)}
 		</div>
 	);
 }
