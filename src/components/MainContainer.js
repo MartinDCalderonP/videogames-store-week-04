654a2f716aeef9fe6f70e90978ecc52edb6b710d
Martín Calderón <martincalderon92@outlook.com>
2021-10-06 22:53:35 -0300

    :sparkles: Add get posts function and render posts.

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/MainContainer.module.scss';
-import Card from './Card';
+import API_KEY from '../Keys';
 import Chevron from './Chevron';
+import Card from './Card';
 
 export default function MainContainer() {
+	const fetchUrl = 'https://api.rawg.io/api/games?page_size=8&key=' + API_KEY;
+	const [posts, setPosts] = useState(undefined);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const getPosts = async () => {
+			setLoading(true);
+			try {
+				const response = await fetch(fetchUrl);
+				const result = await response.json();
+				setPosts(result);
+				setLoading(false);
+			} catch (err) {
+				console.log(`${err}. Try again later.`);
+			}
+		};
+
+		getPosts();
+	}, [fetchUrl]);
+
 	return (
 		<div className={styles.mainContainer}>
 			<Chevron
@@ -11,16 +32,12 @@ export default function MainContainer() {
 				// onClick={handlePreviousClick}
 				orientation="left"
 			/>
-
 			<div className={styles.cardsContainer}>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{loading && <h1>Loading...</h1>}
+
+				{posts?.results.map((post) => (
+					<Card key={post.id} name={post.name} image={post.background_image} />
+				))}
 			</div>
 
 			<Chevron
