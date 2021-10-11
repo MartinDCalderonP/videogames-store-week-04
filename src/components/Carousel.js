eb13ad8f950191da5458c08bb965d68459cd5e38
Martín Calderón <martincalderon92@outlook.com>
2021-10-11 13:34:56 -0300

    :sparkles: Add uppercase title function.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -39,6 +39,20 @@ export default function Carousel({ toDetail }) {
 		toDetail(postId);
 	};
 
+	const uppercaseTitle = (title) => {
+		let words = title.toLowerCase().split(' ');
+
+		for (let i = 0; i < words.length; i++) {
+			if (words[i] === '3d' || words[i] === 'vii') {
+				words[i] = words[i].toUpperCase();
+			} else {
+				words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+			}
+		}
+
+		return words.join(' ');
+	};
+
 	const handlePreviousClick = () => {
 		setCurrent((current) =>
 			current === 0 ? posts?.results.length - 1 : current - 1
@@ -64,13 +78,14 @@ export default function Carousel({ toDetail }) {
 					className={`${styles.carouselItem} ${styles.fade}`}
 					onClick={() => handleCarouselItemClick(posts?.results[current].id)}
 				>
+					<h1>{uppercaseTitle(posts?.results[current].name)}</h1>
 
 					<div className={styles.triangle}></div>
 					<h2>Top Rated </h2>
 
 					<img
 						src={posts?.results[current].background_image}
-						alt={posts?.results[current].background_image}
+						alt={uppercaseTitle(posts?.results[current].name)}
 					/>
 				</div>
 			)}
